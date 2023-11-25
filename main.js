b40db3ee0443a14335c7dede0176eef7c507f5fb
asilbek08ro <146197621+asilbek08ro@users.noreply.github.com>
2023-11-25 11:33:54 +0600

    Update main.js

--- main.js
+++ main.js
@@ -13,11 +13,17 @@ btn.onclick = () => {
         wrapper.style.left = '50%';
         wrapper.style.top = '50%';
         wrapper.style.transform = 'translate(-50%, -50%)';
-
     }
 }
 
+btn2.onclick = () => {
+    let left = Math.round(Math.random() * (90 - 10) + 10) + '%';
+    let top = Math.round(Math.random() * (80 - 20) + 20) + '%';
 
+    wrapper.style.left = left;
+    wrapper.style.top = top;
+    text.style.display = 'none';
+}
 
 var border = document.querySelector('.border');
 
@@ -36,3 +42,4 @@ function changeBorderColor() {
 
 document.getElementById('btn').addEventListener('click', changeBorderColor);
 document.getElementById('btn2').addEventListener('click', changeBorderColor);
+
